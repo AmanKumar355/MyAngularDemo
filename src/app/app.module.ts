152bb210ad3fb7ca2701d4c1f0d4f83e376c890e
unknown <aman141998@gmail.com>
2020-05-16 13:30:30 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddempComponent } from './addemp/addemp.component';
+import { EditempComponent } from './editemp/editemp.component';
+import { ViewallComponent } from './viewall/viewall.component';
+import { ViewbydeptComponent } from './viewbydept/viewbydept.component';
+import { ViewbyidComponent } from './viewbyid/viewbyid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddempComponent,
+    EditempComponent,
+    ViewallComponent,
+    ViewbydeptComponent,
+    ViewbyidComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/emp.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Emp } from './emp';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpService {
+
+  constructor(private http: HttpClient) { }
+
+  public viewAll():Observable<any>{
+    return this.http.get("http://192.168.43.13:8082/springrest/viewall");
+  }
+
+  public viewbyID(eid:number):Observable<any>{
+    return this.http.get("http://192.168.43.13:8082/springrest/viewbyid/" +eid);
+  }
+
+  public viewDepts():Observable<any>{
+    return this.http.get("http://192.168.43.13:8082/springrest/viewdepts");
+  }
+
+  public viewByDept(dname:string):Observable<any>{
+    return this.http.get("http://192.168.43.13:8082/springrest/viewbydept/"+dname);
+  }
+
+  public addEmployee(emp:Emp):Observable<any>{
+    return this.http.post("http://192.168.43.13:8082/springrest/add",emp,{responseType:'text'});
+  }
+
+  public editEmployee(emp:Emp):Observable<any>{
+    return this.http.put("http://192.168.43.13:8082/springrest/edit",emp,{responseType:'text'});
+  }
+
+  public deleteEmployee(eid:number):Observable<any>{
+    return this.http.delete("http://192.168.43.13:8082/springrest/remove/"+eid,{responseType:'text'});
+  }
+}
